9ece7a818b352a39d17b62731bb0f97d7c1e0d6a
Fábio Alves <djfabioalves2002@gmail.com>
2020-07-20 21:04:42 -0100

    Part 1 complete

--- scripts.js
+++ scripts.js
@@ -1,1 +1,46 @@
-var listElement = document.querySelector(#a )
+var listElement = document.querySelector('#app ul')
+var inputElement = document.querySelector('#app input')
+var buttonElement = document.querySelector('#app button')
+
+
+var todos = JSON.parse(localStorage.getItem('listTodo')) || []
+function renderTodos() {
+    listElement.innerHTML = '';
+    for (todo of todos) {
+        var todoElement = document.createElement('li')
+        var todoText = document.createTextNode(todo)
+
+        var linkElement = document.createElement('a')
+        linkElement.setAttribute('href', '#')
+        var linkText = document.createTextNode('Excluir')
+
+        var pos = todos.indexOf(todo)
+        linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')')
+        linkElement.appendChild(linkText)
+
+        todoElement.appendChild(todoText)
+        todoElement.appendChild(linkElement)
+
+        listElement.appendChild(todoElement)
+    }
+}
+renderTodos()
+function addTodo() {
+    var todoText = inputElement.value
+
+    todos.push(todoText)
+    inputElement.value = ''
+    renderTodos()
+    saveToStorage()
+
+}
+buttonElement.onclick = addTodo
+
+function deleteTodo(pos) {
+    todos.splice(pos, 1)
+    renderTodos()
+    saveToStorage()
+}
+function saveToStorage() {
+    localStorage.setItem('listTodo', JSON.stringify(todos))
+}
